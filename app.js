03beb3c425dabb5ece82acb0b295d510112c827c
D.Kerner <denkern@gmail.com>
2017-07-01 19:45:52 +0700

    added partials

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+/*-------------- Routes ---------------*/
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views/pages'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
