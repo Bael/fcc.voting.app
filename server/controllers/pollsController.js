0239cc883f99f59ece4393590c5746e2bf11c94e
D.Kerner <denkern@gmail.com>
2017-07-08 22:56:34 +0700

    styling

--- server/controllers/pollsController.js
+++ server/controllers/pollsController.js
@@ -27,34 +27,36 @@ module.exports.getNewInstance = function (req, res, next) {
 
 /// fill name and options text
 function fillPollInfo(poll, req) {
-      poll.name = req.body.name;
-      
-      for (var i=0; i<req.body.pollOption.length; i++)
-      {
-        var option = req.body.pollOption[i];
-        if (!poll.hasOption(option))
-        {
-          poll.options.push({name: option, votes: 0});
-        }
-      }
+  poll.name = req.body.name;
+
+  for (var i = 0; i < req.body.pollOption.length; i++) {
+    var option = req.body.pollOption[i];
+    if (!poll.hasOption(option)) {
+      poll.options.push({
+        name: option,
+        votes: 0
+      });
+    }
+  }
 }
 
 // save new poll
 module.exports.createNewPoll = function (req, res, next) {
-  
+
   var poll = new Poll({
-     authorId: req.user._id,
+    authorId: req.user._id,
   });
 
   fillPollInfo(poll, req);
-    for (var i=0; i<req.body.pollOption.length; i++)
-      {
-        var option = req.body.pollOption[i];
-        if (!poll.hasOption(option))
-        {
-          poll.options.push({name: option, votes: 0});
-        }
-      }
+  for (var i = 0; i < req.body.pollOption.length; i++) {
+    var option = req.body.pollOption[i];
+    if (!poll.hasOption(option)) {
+      poll.options.push({
+        name: option,
+        votes: 0
+      });
+    }
+  }
 
   poll.save(function (err, created) {
     if (err) {
@@ -84,7 +86,9 @@ module.exports.getById = function (req, res, next) {
 
 // delete poll by id
 module.exports.deleteById = function (req, res, next) {
-  Poll.remove({_id:req.params.id}, function (err, result) {
+  Poll.remove({
+    _id: req.params.id
+  }, function (err, result) {
     if (err) {
       return next(err);
     }
@@ -97,21 +101,25 @@ module.exports.deleteById = function (req, res, next) {
 
 // update poll
 module.exports.updatePoll = function (req, res, next) {
-  console.log("req.params.id is "+ req.params.id);
+  console.log("req.params.id is " + req.params.id);
   console.log(JSON.stringify(res.body));
-  Poll.findById(req.params.id, function (err, founded) 
-  {
+  Poll.findById(req.params.id, function (err, founded) {
     if (err) {
       console.log(err);
       return next(err);
     } else {
 
       fillPollInfo(founded, req);
-      founded.save(function(err, success) {
-        if(err) return next(err);
+      founded.save(function (err, success) {
+        if (err) return next(err);
 
         console.log(JSON.stringify(founded));
-        res.render('poll', { title: 'Poll info' , message:"poll saved!", poll : success, userIsAuthenticatedAndAuthor: (req.isAuthenticated() && success.authorId === req.user._id) });
+        res.render('poll', {
+          title: 'Poll info',
+          message: "poll saved!",
+          poll: success,
+          userIsAuthenticatedAndAuthor: (req.isAuthenticated() && success.authorId === req.user._id)
+        });
       });
     }
   })
@@ -119,19 +127,23 @@ module.exports.updatePoll = function (req, res, next) {
 
 
 // get poll by id
-module.exports.getForVoteById = function (req, res, next) {
-  Poll.findById(req.params.id, function (err, founded) {
+module.exports.getForVoteById = getForVoteById;
+
+function getForVoteById(req, res, next) {
+  Poll.findById(req.params.id, (err, founded) => {
     if (err) {
-      return next(err);
+      next(err);
     } else {
       res.render('pollvote', {
         title: 'Poll vote info',
         message: req.flash('message'),
-        poll: founded
+        poll: founded,
       });
     }
-  })
-};
+  });
+}
+
+
 
 module.exports.getVoteResultById = function (req, res, next) {
   Poll.findById(req.params.id, function (err, founded) {
@@ -139,15 +151,37 @@ module.exports.getVoteResultById = function (req, res, next) {
       return next(err);
     } else {
 
-      var result = {labels:[], data : []};
+      const result = {
+        labels: [],
+        data: []
+      };
 
-      for (var i=0; i<founded.options.length; i++) {
-          result.labels.push(founded.options[i].name);
-          result.data.push(founded.options[i].votes);
+      for (let i = 0; i < founded.options.length; i++) {
+        result.labels.push(founded.options[i].name);
+        result.data.push(founded.options[i].votes);
       }
+
+      
+      result.backgroundColors = ['#1f77b4',
+    '#ff7f0e',
+    '#2ca02c',
+    '#d62728',
+    '#1f77b4',
+    '#ff7f0e',
+    '#2ca02c',
+    '#d62728'];
+
+      /*
+       pattern.generate([
+    '#1f77b4',
+    '#ff7f0e',
+    '#2ca02c',
+    '#d62728'
+    ]);*/
+
       res.send(result);
     }
-  })
+  });
 };
 
 module.exports.vote = function (req, res, next) {
@@ -157,19 +191,27 @@ module.exports.vote = function (req, res, next) {
     } else {
 
       console.log(JSON.stringify(req.body));
-      let userInfo = {ip:req.ip};
+      let userInfo = {
+        ip: req.ip
+      };
 
       if (req.isAuthenticated()) {
-         userInfo.userId = req.user._id; 
+        userInfo.userId = req.user._id;
       }
-      
+
       founded.vote(req.body.pollOption, userInfo);
-      founded.save(function(err, savedPoll) {
-        res.render('pollvoteresult', {
-        title: 'Poll voting info',
-        message: req.flash('message'),
-        poll: savedPoll
-      });
+      founded.save(function (err, savedPoll) {
+        if (err) {
+          console.log("Error ocuured while voting "+err.toString());
+          next(err);
+        }
+        else{ 
+          res.render('pollvoteresult', {
+            title: 'Poll voting info',
+            message: req.flash('message'),
+            poll: savedPoll
+          });
+        }
 
       });
 
